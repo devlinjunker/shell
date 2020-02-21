3cfe56bf4fade42b004b831bcaedbef4654ac41e
Devlin Junker <djunker@C02NQDAVG3QD.local>
2020-02-21 10:12:22 -0800

    update setup script and cleanup gitstatus more so it works when host is not available

--- setup.sh
+++ setup.sh
@@ -1,13 +1,27 @@
 #! /bin/bash
 
-## Copy .bash_profile if ssh into machine. On Windows use su - to change to user account, so need to edit .bashrc that already exists on the windows subsytem
-cp .bash_profile ~ ## should check if exists before copying and append if it does
-source ~/.bash_profile
+changes=()
 
+## Copy .bash_profile if ssh into machine. 
+## On Windows use su - to change to user account, so need to edit .bashrc that already exists on the windows subsytem
+
+## Check if exists before copying (TODO: and append if it does)
+if [[ ! -e ~/.bash_profile ]]; then
+  cp .bash_profile ~ 
+  changes+=("bash_profile")
+fi
+
+if [[ " ${changes[@]} " =~ " bash_profile " ]]; then
+  echo "File added: ~/.bash_profile   ---  run `source ~/.bash_profile` to see the changes"
+fi
+
+
+## Attempt to set basic Vim settings
 ./scripts/bvimrc
 
 
-if [[ -z `which brew` ]]; then
+## Attempt to install stuff with homebrew if installed
+if [[ `which brew` ]]; then
   if [[  -z `which tree` ]]; then
       brew install tree
   fi
@@ -29,3 +43,5 @@ if [[ -z `which brew` ]]; then
   #   color-picker
   #   pigments
 fi
+
+
