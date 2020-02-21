be787339b11572eb24abccf20c00dd35b6707f67
Devlin Junker <devlin.junker@gmail.com>
2020-02-21 11:48:00 -0800

    install homebrew?

--- setup.sh
+++ setup.sh
@@ -21,6 +21,9 @@ fi
 ./scripts/bvimrc
 
 
+## TODO: Git Autocompletion
+# https://github.com/bobthecow/git-flow-completion/wiki/Install-Bash-git-completion
+
 # Advanced Tools
 
 ## OSX
@@ -29,8 +32,7 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
   ## TODO: Should we use macports instead?
   ## Install Homebrew
   if [[ -z `which brew` ]]; then
-    echo "TODO: install homebrew"
-   ## /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   fi
 
   ## Attempt to install stuff with homebrew if installed
@@ -41,6 +43,11 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 
     if [[ -z `which nvm` ]]; then
       brew install nvm
+      
+      ## TODO: place in bash_profile 
+      "export NVM_DIR="$HOME/.nvm"
+      [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+      [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion" 
     fi
   fi
 fi
