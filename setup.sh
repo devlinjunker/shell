c8fcde418d37b86edb16f9ebd093704fd244133b
Devlin Junker <devlin.junker@gmail.com>
2020-02-21 10:56:24 -0800

    more thoughts on what to do in setup

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
 
+# Settings changes
+
 changes=()
 
 ## Copy .bash_profile if ssh into machine. 
 ## On Windows use su - to change to user account, so need to edit .bashrc that already exists on the windows subsytem
-
 ## Check if exists before copying (TODO: and append if it does)
 if [[ ! -e ~/.bash_profile ]]; then
   cp .bash_profile ~ 
@@ -20,28 +21,44 @@ fi
 ./scripts/bvimrc
 
 
-## Attempt to install stuff with homebrew if installed
-if [[ `which brew` ]]; then
-  if [[  -z `which tree` ]]; then
-      brew install tree
+# Advanced Tools
+
+## OSX
+if [[ "$OSTYPE" == "darwin"* ]]; then
+
+  ## TODO: Should we use macports instead?
+  ## Install Homebrew
+  if [[ -z `which brew` ]]; then
+    echo "TODO: install homebrew"
+   ## /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   fi
 
-  #install atom
-  #   nuclide
-  #   git log
-  #   atom-ternjs
-  #   language-babel (or react?) https://orktes.github.io/atom-react/#syntax-highlighting
-  #   highlight selected?
-  #   linter-eslint (ignore linter dependency)
-  #   js-refactor
-  #   prettier-atom
-  #   mocha-test-runner
+  ## Attempt to install stuff with homebrew if installed
+  if [[ `which brew` ]]; then
+    if [[  -z `which tree` ]]; then
+        brew install tree
+    fi
 
-  ### MacMini
-  #   atom-html-preview 
-  #   autocomplete-modules
-  #   color-picker
-  #   pigments
+    if [[ -z `which nvm` ]]; then
+      brew install nvm
+    fi
+  fi
 fi
 
+#TODO: install atom
+#   nuclide
+#   git log
+#   atom-ternjs
+#   language-babel (or react?) https://orktes.github.io/atom-react/#syntax-highlighting
+#   highlight selected?
+#   linter-eslint (ignore linter dependency)
+#   js-refactor
+#   prettier-atom
+#   mocha-test-runner
+
+### MacMini
+#   atom-html-preview 
+#   autocomplete-modules
+#   color-picker
+#   pigments
 
